712c158a69982946f257ba47089c81bcf76b9f62
Gangadhar Gottiveeti <gangadhar9398645347@gmail.com>
2024-12-12 11:47:45 +0530

    Add. intitial SensorDataConsumer application setup

---
+++ src/test/java/com/example/SensorDataConsumer/SensorDataConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SensorDataConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorDataConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
